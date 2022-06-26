e081f9a157680b8236176f50dac5095c32ba139e
jairomaru <jairomura@hotmail.com>
2022-06-26 11:03:41 -0300

    primeiro

---
+++ atividadeencontro2.js
@@ -0,0 +1,15 @@
+
+let alunosPresentes= 17;
+
+for(let aluno = 0; aluno <= alunosPresentes; aluno++) {
+  
+if(aluno == 0 ){
+    console.log("zero"+ aluno);
+}
+
+  if(aluno % 2==0){
+     console.log("par"+ aluno);
+  }else{
+    console.log(`ímpar${aluno}`);
+  }
+}  
